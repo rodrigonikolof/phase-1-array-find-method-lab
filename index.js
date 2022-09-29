ce06aabe6d20cadbaf14b7153cbb2fce18715422
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-09-29 20:15:51 +1000

    commit tree 2905d1f2283f4b86ca467c0bcf6e9dc9d1ed9db8

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+
+
+// const record = [
+//     { year: "2018", result: "N/A"},
+//     { year: "2017", result: "N/A"},
+//     { year: "2016", result: "N/A"},
+//     { year: "2015", result: "W"}]
+
+
+
+function superbowlWin(record) {
+    let yearWon = record.find(year => year.result === 'W');
+     return yearWon? yearWon.year : undefined; 
+}
+
+
+
+// console.log(superbowlWin(record));
